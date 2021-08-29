b7c47446f9bb8bc00a826d0f963d4b03888ab5a2
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-08-29 15:21:18 +0900

    イベントハンドラを追加

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,9 +1,15 @@
-import { VFC } from "react";
+import { FormEvent, useState, VFC } from "react";
 
 export const NewTodo: VFC = () => {
+    const [title, setTitle] = useState<string>("");
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
     return (
-        <form>
-            <input type="text" name="title" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="title" onChange={(event) => setTitle(event.currentTarget.value)} value={title}/>
             <button type="submit">追加</button>
         </form>
     );
