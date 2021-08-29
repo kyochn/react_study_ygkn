3495879d82c7d78316854de91679b717a47965f2
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-08-29 16:06:50 +0900

    リストに追加されるようにした

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,31 @@
-import { VFC } from "react";
+import { useState, VFC } from "react";
+import { Todo } from "../lib/todo";
 import { NewTodo } from "./NewTodo";
 
 export const App: VFC = () => {
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+
     return (
         <div>
             <NewTodo
-                onAdd={(title) => console.log(title)}
+                onAdd={(title) => [
+                    setTodoList((oldTodoList) => [
+                        ...oldTodoList,
+                        {
+                            title,
+                            done: false,
+                            id: Math.random().toString(),
+                        },
+                    ])
+                ]}
             />
+            <ul>
+                {
+                    todoList.map((todo) => (
+                        <li key={todo.id}>{todo.title}</li>
+                    ))
+               } 
+            </ul>
         </div>
     );
 };
