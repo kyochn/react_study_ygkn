fe96025b64f196d928c00d6c71b7ffd152dd0640
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-08-29 15:23:55 +0900

    コメント文を追加

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -4,12 +4,16 @@ export const NewTodo: VFC = () => {
     const [title, setTitle] = useState<string>("");
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+        event.preventDefault(); //フォームの送信を止める
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" name="title" onChange={(event) => setTitle(event.currentTarget.value)} value={title}/>
+            <input type="text"
+                name="title"
+                onChange={(event) => setTitle(event.currentTarget.value)} //input が変更されたら 'title' state を変更
+                value={title} //value をtitle state に
+            />
             <button type="submit">追加</button>
         </form>
     );
